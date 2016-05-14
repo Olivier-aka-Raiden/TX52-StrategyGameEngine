d0ea701507127c5977e5f872ea7e9cd5236dce4d
Olivier VILLEQUEY <olivier.villequey@utbm.fr>
2016-05-14 17:02:23 +0200

    ajout de environmentEvent

--- src/main/java/tx52/environment/EnvironmentEvent.java
+++ src/main/java/tx52/environment/EnvironmentEvent.java
@@ -1,5 +1,48 @@
 package tx52.environment;
 
-public class EnvironmentEvent {
+import java.util.EventObject;
+import java.util.Map;
+import java.util.UUID;
+
+import org.arakhne.afc.math.continous.object2d.Point2f;
+
+public class EnvironmentEvent extends EventObject {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final int time;
+	
+	private final int width;
+	
+	private final int height;
+
+	private final Map<Point2f, EnvironmentObject> objects;
+
+	public EnvironmentEvent(UUID source, int time, int width, int height, Map<Point2f, EnvironmentObject> objects) {
+		super(source);
+		this.time = time;
+		this.width = width;
+		this.height = height;
+		this.objects = objects;
+	}
+	
+	public int getTime() {
+		return this.time;
+	}
+	public int getWidth() {
+		return this.width;
+	}
+	
+	public int getHeight() {
+		return this.height;
+	}
+
+	public Map<Point2f, EnvironmentObject> getObjects() {
+		return this.objects;
+	}
 
 }
