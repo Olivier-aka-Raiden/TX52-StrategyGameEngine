b6534878a9421e8d7e5fd3854bfcbd42659f2980
Olivier VILLEQUEY <olivier.villequey@utbm.fr>
2016-06-08 23:29:17 +0200

    plus de lags!

--- src/main/java/tx52/gui/Unit.java
+++ src/main/java/tx52/gui/Unit.java
@@ -3,6 +3,7 @@ package tx52.gui;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.UUID;
@@ -24,35 +25,18 @@ public class Unit {
 		position = a.getPosition();
 		team = a.getTeam();
 		id = a.getId();
-		
 	}
 	
 	public void setPosition(Point2f position) {
 		this.position = position;
 	}
 
-	public void paint(Graphics g, JPanel container)
+	public void paint(Graphics g,BufferedImage image, JPanel container)
 	{
 		Graphics2D g2d = (Graphics2D) g;
-		try
-		{
-			// Resources dans AFC vmutils
-			URL url;
-			if (this.team == 1){
-				url = getClass().getClassLoader().getResource("images/unit.png");
-			} else {
-				url = getClass().getClassLoader().getResource("images/unit2.png");
-			}
-	    	assert (url != null);
-			Image img = ImageIO.read(url);
-			float posX = position.getX() * 5;
-			float posY = position.getY() * 5;
-			g2d.drawImage(img, (int) posX, (int) posY, container);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+		float posX = position.getX() * 5;
+		float posY = position.getY() * 5;
+		g2d.drawImage(image, (int) posX, (int) posY, container);
 	}
 	
 }
